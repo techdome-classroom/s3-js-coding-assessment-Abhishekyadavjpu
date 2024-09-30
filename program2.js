b87cd30b9046b77c29cfbf8cda3768d55d6fcfdd
Abhishekyadavjpu <abhishekyadavjpu@gmail.com>
2024-09-30 16:03:40 +0530

    third

--- program2.js
+++ program2.js
@@ -3,8 +3,42 @@
  * @return {number}
  */
 var romanToInt = function(s) {
+   
+       
+        const romanMap = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        };
     
-};
+        let total = 0;
+    
+       
+        for (let i = 0; i < s.length; i++) {
+            const current = romanMap[s[i]];
+            const next = romanMap[s[i + 1]];
+    
+          
+            if (current < next) {
+                total -= current;
+            } else {
+                total += current;
+            }
+        }
+    
+        return total;
+    }
+    
+    
+    console.log(romanToInt("III"));     
+    console.log(romanToInt("LVIII"));    
+    console.log(romanToInt("MCMXCIV"));  
+    
+
 
 
 module.exports={romanToInt}
